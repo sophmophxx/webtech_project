eac7e993662fdda3a01086c8550612230ab7636d
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2026-07-30 16:56:24 +0200

    chore: add RouterLink import

--- frontend/src/app/features/wardrobe/pages/wardrobe-page/wardrobe-page.ts
+++ frontend/src/app/features/wardrobe/pages/wardrobe-page/wardrobe-page.ts
@@ -6,10 +6,11 @@ import { ClothingItemService } from '../../../../core/services/clothing-item.ser
 import { ClothingItem } from '../../../../shared/models/clothing-item.model';
 import { ClothingItemCard } from '../../components/clothing-item-card/clothing-item-card';
 import { MatButtonModule } from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-wardrobe-page',
-  imports: [MatButtonModule, MatIconModule, MatProgressSpinnerModule, ClothingItemCard],
+  imports: [RouterLink, MatButtonModule, MatIconModule, MatProgressSpinnerModule, ClothingItemCard],
   templateUrl: './wardrobe-page.html',
   styleUrl: './wardrobe-page.scss',
 })
