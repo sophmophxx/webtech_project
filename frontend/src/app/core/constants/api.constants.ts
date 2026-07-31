5bb6cb4c648ae27eaf9507749ba1fd70a809edfe
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2026-07-31 15:10:27 +0200

    refactor: remove unused health api constant

--- frontend/src/app/core/constants/api.constants.ts
+++ frontend/src/app/core/constants/api.constants.ts
@@ -3,4 +3,3 @@ import { environment } from '../../../environments/environment';
 export const API_BASE_URL = environment.apiBaseUrl;
 
 export const ITEMS_API_URL = `${API_BASE_URL}/items`;
-export const HEALTH_API_URL = `${API_BASE_URL}/health`;
