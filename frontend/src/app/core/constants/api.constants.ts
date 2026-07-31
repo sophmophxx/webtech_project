6d283f4e8c9cf1f3089930c5391defcd4bd2a7f9
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2026-07-31 21:22:48 +0200

    feat: add outfit frontend data layer

--- frontend/src/app/core/constants/api.constants.ts
+++ frontend/src/app/core/constants/api.constants.ts
@@ -3,3 +3,4 @@ import { environment } from '../../../environments/environment';
 export const API_BASE_URL = environment.apiBaseUrl;
 
 export const ITEMS_API_URL = `${API_BASE_URL}/items`;
+export const OUTFITS_API_URL = `${API_BASE_URL}/outfits`;
